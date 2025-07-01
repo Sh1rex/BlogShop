226aca50159ceb92c40fd00d250c6720ac3f0107
Maksym <maxim03102007@gmail.com>
2025-07-01 22:36:23 +0200

    added urls from conrib.auth.urls

--- BlogShop/BlogShop/urls.py
+++ BlogShop/BlogShop/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
+    path('users/', include('django.contrib.auth.urls')),
     path('', include('mainpage.urls', namespace='main_page')),
 ]
