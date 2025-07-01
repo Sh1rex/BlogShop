92c97ff91bf2856e371af83ea8c3e2c8030bbd67
Maksym <maxim03102007@gmail.com>
2025-07-01 22:39:01 +0200

    added authentication

--- BlogShop/users/urls.py
+++ BlogShop/users/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls')),
     path('profile/', views.profile, name='profile'),
     path('register/', views.register, name='register'),
 ]
--- BlogShop/users/views.py
+++ BlogShop/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationForm
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('users:login')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {
@@ -16,4 +16,4 @@ def register(request):
 def profile(request):
     if request.user.is_authenticated:
         return render(request, 'users/profile.html')
-    return redirect('users:login')
+    return redirect('login')
