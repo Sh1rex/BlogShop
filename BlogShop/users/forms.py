43f5d10e7c1b55e081e33253bba3f07121688030
Maksym <maxim03102007@gmail.com>
2025-07-07 23:35:37 +0200

    form for upload new imagine of profile

--- BlogShop/users/forms.py
+++ BlogShop/users/forms.py
@@ -1,8 +1,15 @@
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 class UserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        exclude = ['user']
