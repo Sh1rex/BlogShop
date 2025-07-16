d1dd935ebbe673c9c822882e6dcad2a98939d9be
Maksym <maxim03102007@gmail.com>
2025-07-16 15:19:05 +0200

    urls for subscribers/subscribed

--- BlogShop/users/urls.py
+++ BlogShop/users/urls.py
@@ -4,8 +4,14 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('profile/', views.own_profile, name='own_profile'),
-    path('profile/<slug:slug>/', views.profile, name='profile'),
-    path('register/', views.register, name='register'),
-    path('settings/', views.settings, name='settings')
+    path('settings/', views.settings, name='settings'),
+    path('register', views.register, name='register'),
+    path('profile/', include([
+        path('', views.own_profile, name='own_profile'),
+        path('<slug:slug>/', include([
+            path('', views.profile, name='profile'),
+            path('subscribers/', views.subscribers, name='subscribers'),
+            path('subscribed/', views.subscribed, name='subscribed'),
+        ])),
+    ]))
 ]
