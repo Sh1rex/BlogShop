831a7c8093faaccf44670583e869b3431ab407cc
Maksym <maxim03102007@gmail.com>
2025-08-05 02:25:33 +0200

    added image field for Post and def get_absolute_url for Category

--- BlogShop/blog/models.py
+++ BlogShop/blog/models.py
@@ -7,14 +7,18 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=200, unique=True)
 
+    def get_absolute_url(self):
+        return reverse('mainpage:recommendations_by_category', args=[self.slug])
+
     def __str__(self):
         return self.name
 
 class Post(models.Model):
     seller = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
-    product = models.CharField(max_length=50)
-    slug = models.SlugField(max_length=200)
+    product = models.CharField(max_length=50, blank=False, null=False)
+    slug = models.SlugField(max_length=200, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='posts/', null=True)
     description = models.TextField()
     price = models.DecimalField(decimal_places=2,
                                 max_digits=10)
